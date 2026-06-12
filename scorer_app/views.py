907d91546f02d231bd35a71463a79cec9deff4b6
ekuprianova18-wq <ekuprianova18@gmail.com>
2026-06-12 17:15:52 +0500

    Add main page with repository list

--- scorer_app/views.py
+++ scorer_app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import Repository, ReliabilityScore
+
+
+def index(request):
+    repos = Repository.objects.all()
+
+    repos_data = []
+    for repo in repos:
+        try:
+            score_obj = repo.reliability_score
+            total_score = score_obj.total_score
+        except ReliabilityScore.DoesNotExist:
+            total_score = None
+
+        repos_data.append({
+            'repo': repo,
+            'score': total_score,
+        })
+
+    repos_data.sort(key=lambda x: x['score'] if x['score'] is not None else -1, reverse=True)
+
+    return render(request, 'scorer_app/index.html', {'repos_data': repos_data})
